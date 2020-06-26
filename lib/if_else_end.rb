ee71285a940094240977a144e52054b27b1d7782
Noa Rabin-Cohen <cohenoa33@gmail.com>
2020-06-26 20:54:56 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,5 +1,32 @@
+# Write your solution here
+run_code_inside = false
+puts "Code before if...end"
+if run_code_inside
+  puts "code inside"
+end
+puts "Code after if...end"
+
+#if else end
+chance_of_rain = 0.2
+puts "Let's go outside!"
+if chance_of_rain > 0.5
+  puts "Pack an umbrella!"
+else 
+  puts "Enjoy the fine day!"
+end
+puts "Oh, and always wear sunscreen!"
+
+#if elsif else end
+chance_of_rain = -23
+if chance_of_rain <= 0.25
+  puts "Pack a sun Shelter!"
+elsif (chance_of_rain > 0.25) && (chance_of_rain < 0.75)
+  puts "Pack an umbrella!"
+else
+puts "Stay home and read Hegel"
+end
 
 
 this_year = 2019 
-puts "Hey, it's 2019!" 
-unless this_year == 2019
+puts "Hey, it's 2019!" if
+this_year == 2019
